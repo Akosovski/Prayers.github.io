ea2d4f959be1bce09d4c235ade4826d7494451dc
Muhammad Akhta <66517802+Akosovski@users.noreply.github.com>
2022-03-28 14:26:20 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let yaranai = new Audio("./sound/Yaranai.mp3");
+let kyofuuyo = new Audio("./sound/Kyofuuni.mp3");
+let jibun_no_unmei = new Audio("./sound/Jibun no Unmei.mp3");
+let raimeini = new Audio("./sound/Raimei ni.mp3");
+let watashi = new Audio("./sound/Watashi wa sentoku.mp3");
+let kaminari = new Audio("./sound/Kaminari no Ritsusha.mp3");
+let yami = new Audio("./sound/Yami ni ochi.mp3");
+
+function playKyofuuyo() {
+    kyofuuyo.volume = 0.5;
+    kyofuuyo.play();
+}
+
+function playJibun() {
+    jibun_no_unmei.volume = 0.5;
+    jibun_no_unmei.play();
+}
+
+function playRaimeini() {
+    raimeini.volume = 0.5;
+    raimeini.play();
+}
+
+function playWatashi() {
+    watashi.volume = 0.5;
+    watashi.play();
+}
+
+function playKaminari() {
+    kaminari.volume = 0.5;
+    kaminari.play();
+}
+
+function playYami() {
+    yami.volume = 0.5;
+    yami.play();
+}
+
+function playYaranai() {
+    yaranai.volume = 0.5;
+    yaranai.play();
+}
