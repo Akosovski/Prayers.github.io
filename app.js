37ada5cf5f8a73e0f1b140d38958d9515faa17eb
Muhammad Akhta <akhtacaesar@gmail.com>
2022-07-21 16:39:38 +0700

    Added Done Button to Cards

--- app.js
+++ app.js
@@ -1,42 +1,9 @@
-let yaranai = new Audio("./sound/Yaranai.mp3");
-let kyofuuyo = new Audio("./sound/Kyofuuni.mp3");
-let jibun_no_unmei = new Audio("./sound/Jibun no Unmei.mp3");
-let raimeini = new Audio("./sound/Raimei ni.mp3");
-let watashi = new Audio("./sound/Watashi wa sentoku.mp3");
-let kaminari = new Audio("./sound/Kaminari no Ritsusha.mp3");
-let yami = new Audio("./sound/Yami ni ochi.mp3");
+let element1 = document.getElementById("btndoa1");
+element1.addEventListener("click", done1);
 
-function playKyofuuyo() {
-    kyofuuyo.volume = 0.5;
-    kyofuuyo.play();
-}
-
-function playJibun() {
-    jibun_no_unmei.volume = 0.5;
-    jibun_no_unmei.play();
-}
-
-function playRaimeini() {
-    raimeini.volume = 0.5;
-    raimeini.play();
-}
-
-function playWatashi() {
-    watashi.volume = 0.5;
-    watashi.play();
-}
-
-function playKaminari() {
-    kaminari.volume = 0.5;
-    kaminari.play();
-}
-
-function playYami() {
-    yami.volume = 0.5;
-    yami.play();
-}
-
-function playYaranai() {
-    yaranai.volume = 0.5;
-    yaranai.play();
+function done1() {
+    document.getElementById("doa1").style.background = "#9d9d9d";
+    document.getElementById("doaimg1").style.filter = "brightness(70%)";
+    element1.remove('btn-primary');
+    element1.toggle('btn-success');
 }
