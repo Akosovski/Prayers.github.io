5a82975de56a8c89c80f508444ba6b9a37277ff5
Muhammad Akhta <akhtacaesar@gmail.com>
2022-08-04 22:38:33 +0700

    Updated Finish Button to All Cards

--- app.js
+++ app.js
@@ -77,3 +77,103 @@ function done8() {
     element8.remove('btn-primary');
     element8.toggle('btn-success');
 }
+
+let element9 = document.getElementById("btndoa9");
+element9.addEventListener("click", done9);
+
+function done9() {
+    document.getElementById("doa9").style.background = "#9d9d9d";
+    document.getElementById("doaimg9").style.filter = "brightness(80%)";
+    element9.remove('btn-primary');
+    element9.toggle('btn-success');
+}
+
+let element10 = document.getElementById("btndoa10");
+element10.addEventListener("click", done10);
+
+function done10() {
+    document.getElementById("doa10").style.background = "#9d9d9d";
+    document.getElementById("doaimg10").style.filter = "brightness(80%)";
+    element10.remove('btn-primary');
+    element10.toggle('btn-success');
+}
+
+let element11 = document.getElementById("btndoa11");
+element11.addEventListener("click", done11);
+
+function done11() {
+    document.getElementById("doa11").style.background = "#9d9d9d";
+    document.getElementById("doaimg11").style.filter = "brightness(80%)";
+    element11.remove('btn-primary');
+    element11.toggle('btn-success');
+}
+
+let element12 = document.getElementById("btndoa12");
+element12.addEventListener("click", done12);
+
+function done12() {
+    document.getElementById("doa12").style.background = "#9d9d9d";
+    document.getElementById("doaimg12").style.filter = "brightness(80%)";
+    element12.remove('btn-primary');
+    element12.toggle('btn-success');
+}
+
+let element13 = document.getElementById("btndoa13");
+element13.addEventListener("click", done13);
+
+function done13() {
+    document.getElementById("doa13").style.background = "#9d9d9d";
+    document.getElementById("doaimg13").style.filter = "brightness(80%)";
+    element13.remove('btn-primary');
+    element13.toggle('btn-success');
+}
+
+let element14 = document.getElementById("btndoa14");
+element14.addEventListener("click", done14);
+
+function done14() {
+    document.getElementById("doa14").style.background = "#9d9d9d";
+    document.getElementById("doaimg14").style.filter = "brightness(80%)";
+    element14.remove('btn-primary');
+    element14.toggle('btn-success');
+}
+
+let element15 = document.getElementById("btndoa15");
+element15.addEventListener("click", done15);
+
+function done15() {
+    document.getElementById("doa15").style.background = "#9d9d9d";
+    document.getElementById("doaimg15").style.filter = "brightness(80%)";
+    element15.remove('btn-primary');
+    element15.toggle('btn-success');
+}
+
+let element16 = document.getElementById("btndoa16");
+element16.addEventListener("click", done16);
+
+function done16() {
+    document.getElementById("doa16").style.background = "#9d9d9d";
+    document.getElementById("doaimg16").style.filter = "brightness(80%)";
+    element16.remove('btn-primary');
+    element16.toggle('btn-success');
+}
+
+let element17 = document.getElementById("btndoa17");
+element17.addEventListener("click", done17);
+
+function done17() {
+    document.getElementById("doa17").style.background = "#9d9d9d";
+    document.getElementById("doaimg17").style.filter = "brightness(80%)";
+    element17.remove('btn-primary');
+    element17.toggle('btn-success');
+}
+
+let element18 = document.getElementById("btndoa18");
+element18.addEventListener("click", done18);
+
+function done18() {
+    document.getElementById("doa18").style.background = "#9d9d9d";
+    document.getElementById("doaimg18").style.filter = "brightness(80%)";
+    element18.remove('btn-primary');
+    element18.toggle('btn-success');
+}
